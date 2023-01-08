ac715c8fb899b0aa4241ea751cdd33adeaf00840
martinvrbka <martin.vrbkaa@seznam.cz>
2023-01-08 20:43:00 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+choice = input("Do you wanna go left or right?")
+
+if choice == "left":
+    choice = input("You got to the lake do you want to swim or wait for the boat?")
+    if choice == "wait":
+        choice = input("You waited and boat came to pick you. The captain took you to harbor with three doors Red, Blue and Yellow. Which one you choose?")
+        if choice == "Yellow":
+            print("You chose the right door. Hwn you open the yellow door you are blinded by the blinking light from the huge treasure you just found. You Win!")
+        else:
+            print("Wrong door sucker. Game over.")
+    else:
+        print("You drowned while swimming. Game over.")
+else:
+    print("You went right and hit the wall with your head and died. Game over.")
+
